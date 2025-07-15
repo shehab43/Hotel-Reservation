422629c188a2a12df2114b8a11edbf151c982802
shehab43 <Shehabmohamed6n789@gmail.com>
2025-07-15 17:45:05 +0300

    1-AddInfrastructureLayer 2-AddRequestContextLoggingMiddleware 3-Add Aplication/Abstraction/Auth

---
+++ Application/Abstractions/Authentication/ITokenProvider.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.Authentication
+{
+    public interface ITokenProvider
+    {
+        string Create(User user);
+    }
+}
---
+++ Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Database
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+   
+}
